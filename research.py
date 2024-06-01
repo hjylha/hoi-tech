12c630227cb300dff7edbab05ab22eb54f6d1123
Heikki <hjjylh@gmail.com>
2024-06-01 22:21:20 +0300

    added a bunch of stuff to research.py

--- research.py
+++ research.py
@@ -1,18 +1,65 @@
 
-from scan_hoi_files import get_country_names, scan_techs, get_tech_teams
+from scan_hoi_files import get_country_names, scan_techs, get_tech_teams, scan_scenario_file_for_country
 
 
 class Research:
 
-    def __init__(self, research_speed=100, difficulty=0, list_of_techs=None, countries=None) -> None:
-        if list_of_techs is None:
-            list_of_techs = scan_techs()
-        self.techs = {tech.tech_id: tech for tech in list_of_techs}
+    def are_tech_requirements_completed(self, tech_id):
+        if not self.techs[tech_id].requirements:
+            return True
+        for req in self.techs[tech_id].requirements:
+            if isinstance(req, int):
+                if req in self.completed_techs:
+                    return True
+                return False
+            if isinstance(req, list):
+                for r in req:
+                    if r in self.completed_techs:
+                        return True
+                return False
+
+    def clear_all_tech(self):
         self.completed_techs = set()
         self.techs_researching = set()
         self.active_techs = set()
         self.deactivated_techs = set()
-        self.research_speed = research_speed
+        self.blueprints = set()
+
+    def choose_primary_country(self, country_code):
+        self.primary_country = country_code
+        scenario_data = scan_scenario_file_for_country(country_code)
+        if scenario_data is None:
+            return
+        self.completed_techs = set(scenario_data["researched"])
+        self.deactivated_techs = set(scenario_data["deactivated"])
+        self.blueprints = set(scenario_data["blueprints"])
+        self.research_speed = scenario_data["research_speed"]
+
+        for tech_id in self.techs:
+            if self.are_tech_requirements_completed(tech_id) and tech_id not in self.completed_techs and tech_id not in self.deactivated_techs:
+                self.active_techs.add(tech_id)
+
+
+    def __init__(self, research_speed=None, difficulty=0, list_of_techs=None, countries=None, year=1933) -> None:
+        if list_of_techs is None:
+            list_of_techs = scan_techs()
+        self.techs = {tech.tech_id: tech for tech in list_of_techs}
+
+        self.primary_country = None
+
+        self.clear_all_tech()
+
+        self.year = year
+        self.research_speed = 100
+
+        if countries:
+            country_code = countries[0]
+            self.choose_primary_country(country_code)
+            
+        
+        # can override research speed
+        if research_speed is not None:
+            self.research_speed = research_speed
         self.countries = [] if countries is None else countries
 
         self.teams = []
@@ -24,10 +71,16 @@ class Research:
     def add_country(self, country_code):
         self.countries.append(country_code)
         self.teams += get_tech_teams(country_code)
+        if self.primary_country is None:
+            self.choose_primary_country(country_code)
+
     
     def remove_country(self, country_code):
         self.countries.remove(country_code)
         self.teams = [team for team in self.teams if team.nation != country_code]
+        if self.primary_country == country_code:
+            self.primary_country = None
+            self.clear_all_tech()
 
     def find_tech(self, search_term):
         results = []
@@ -47,20 +100,6 @@ class Research:
     def print_active_tech(self):
         for tech_id in self.active_techs:
             print(self.techs[tech_id])
-
-    def are_tech_requirements_completed(self, tech_id):
-        if not self.techs[tech_id].requirements:
-            return True
-        for req in self.techs[tech_id].requirements:
-            if isinstance(req, int):
-                if req in self.completed_techs:
-                    return True
-                return False
-            if isinstance(req, list):
-                for r in req:
-                    if r in self.completed_techs:
-                        return True
-                return False
 
     def activate_tech(self, tech_id):
         self.active_techs.add(tech_id)
@@ -96,9 +135,12 @@ class Research:
         # OTHER STUFF????????
     
     def sort_teams_for_researching_tech(self, tech):
+        # check blueprint
+        has_blueprint = int(tech.tech_id in self.blueprints)
+
         team_results = []
         for team in self.teams:
-            days = team.calculate_how_many_days_to_complete(tech, self.research_speed)
+            days = team.calculate_how_many_days_to_complete(tech, self.research_speed, has_blueprint)
             team_results.append([team, days])
         return sorted(team_results, key=lambda x: x[1])
     
@@ -118,10 +160,18 @@ if __name__ == "__main__":
         if nation_code == country.upper() or nation.upper() == country.upper():
             country_code = nation_code
             print(f"Selected: {nation_code} {nation}")
-    research_speed = input("Research speed (100 if skipped):")
-    if research_speed:
-        research_speed = float(research_speed)
-    else:
-        research_speed = 100
+            break
+    # research_speed = input("Research speed (100 if skipped):")
+    # if research_speed:
+    #     research_speed = float(research_speed)
+    # else:
+    #     research_speed = 100
 
-    r = Research(research_speed, countries=[country_code])
+    # r = Research(research_speed, countries=[country_code])
+    r = Research(countries=[country_code])
+    print("Research speed:", r.research_speed)
+    print("Completed:", r.completed_techs)
+    print("Deactivated:", r.deactivated_techs)
+    print("Blueprints:", r.blueprints)
+
+
