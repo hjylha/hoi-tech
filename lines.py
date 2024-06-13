8e8632d51ef0d5d0879f61607aca8252cf952811
Heikki <hjjylh@gmail.com>
2024-06-13 11:05:45 +0300

    forgot to add lines

---
+++ lines.py
@@ -0,0 +1,14 @@
+
+right1_x = 0.14
+left1_x = 0.2
+
+
+infantry_lines = [
+    # machineguns -> infantry
+    (right1_x, 0.925, left1_x, 0.9),
+    (right1_x, 0.89, left1_x, 0.86),
+    (right1_x, 0.855, left1_x, 0.82),
+    (right1_x, 0.82, left1_x, 0.78),
+    (right1_x, 0.785, left1_x, 0.74),
+    (right1_x, 0.75, left1_x, 0.7)
+]
