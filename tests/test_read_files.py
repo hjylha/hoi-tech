1624f54ed1248d28ebe1362773dcc459a4812763
Heikki Jylhä <hjjylh@gmail.com>
2025-11-26 16:12:09 +0200

    moved read_csv_file and read_txt_file to read_files.py

--- tests/test_read_files.py
+++ tests/test_read_files.py
@@ -4,8 +4,12 @@ from pathlib import Path
 import pytest
 
 import fix_imports
-import read_hoi_files as rhf
+import read_files as rf
 
+
+@pytest.fixture
+def text_encoding():
+    return "ISO-8859-1"
 
 @pytest.fixture
 def txt_file_content0():
@@ -92,7 +96,7 @@ def txt_file_content_list2():
         ]
 )
 def test_change_type_if_necessary(text, result):
-    assert rhf.change_type_if_necessary(text) == result
+    assert rf.change_type_if_necessary(text) == result
 
 
 @pytest.mark.parametrize(
@@ -109,44 +113,44 @@ def test_change_type_if_necessary(text, result):
     ]
 )
 def test_get_first_item_from_text(text, first_item, the_rest):
-    assert rhf.get_first_item_from_text(text) == (first_item, the_rest)
+    assert rf.get_first_item_from_text(text) == (first_item, the_rest)
 
 def test_get_first_item_from_text_iterative():
     full_line = "{1 2 3 4}"
-    item0, line0 = rhf.get_first_item_from_text(full_line)
+    item0, line0 = rf.get_first_item_from_text(full_line)
     assert item0 == "{"
     assert line0 == "1 2 3 4}"
-    item1, line1 = rhf.get_first_item_from_text(line0)
+    item1, line1 = rf.get_first_item_from_text(line0)
     assert item1 == 1
     assert line1 == "2 3 4}"
-    item2, line2 = rhf.get_first_item_from_text(line1)
+    item2, line2 = rf.get_first_item_from_text(line1)
     assert item2 == 2
     assert line2 == "3 4}"
-    item3, line3 = rhf.get_first_item_from_text(line2)
+    item3, line3 = rf.get_first_item_from_text(line2)
     assert item3 == 3
     assert line3 == "4}"
-    item4, line4 = rhf.get_first_item_from_text(line3)
+    item4, line4 = rf.get_first_item_from_text(line3)
     assert item4 == 4
     assert line4 == "}"
 
 def test_get_first_item_from_text_iterative2():
     full_line = "key1 = 0 key1 = 1"
-    item0, line0 = rhf.get_first_item_from_text(full_line)
+    item0, line0 = rf.get_first_item_from_text(full_line)
     assert item0 == "key1"
     assert line0 == "= 0 key1 = 1"
-    item1, line1 = rhf.get_first_item_from_text(line0)
+    item1, line1 = rf.get_first_item_from_text(line0)
     assert item1 == "="
     assert line1 == "0 key1 = 1"
-    item2, line2 = rhf.get_first_item_from_text(line1)
+    item2, line2 = rf.get_first_item_from_text(line1)
     assert item2 == 0
     assert line2 == "key1 = 1"
-    item3, line3 = rhf.get_first_item_from_text(line2)
+    item3, line3 = rf.get_first_item_from_text(line2)
     assert item3 == "key1"
     assert line3 == "= 1"
-    item4, line4 = rhf.get_first_item_from_text(line3)
+    item4, line4 = rf.get_first_item_from_text(line3)
     assert item4 == "="
     assert line4 == "1"
-    item5, line5 = rhf.get_first_item_from_text(line4)
+    item5, line5 = rf.get_first_item_from_text(line4)
     assert item5 == 1
     assert line5 == ""
 
@@ -154,92 +158,92 @@ def test_get_first_item_from_text_iterative2():
 # def test_read_txt_file00(txt_file_content0):
 #     test_txt_file_path = Path(__file__).parent / "txt_file_for_testing0.txt"
 #     assert test_txt_file_path.exists()
-#     content = rhf.read_txt_file0(test_txt_file_path)
+#     content = rf.read_txt_file0(test_txt_file_path)
 #     assert isinstance(content, dict)
 #     assert content == txt_file_content0
 
 # def test_read_txt_file01(txt_file_content1):
 #     test_txt_file_path = Path(__file__).parent / "txt_file_for_testing1.txt"
 #     assert test_txt_file_path.exists()
-#     content = rhf.read_txt_file0(test_txt_file_path)
+#     content = rf.read_txt_file0(test_txt_file_path)
 #     assert isinstance(content, dict)
 #     assert content == txt_file_content1
 
 # def test_read_txt_file02(txt_file_content2):
 #     test_txt_file_path = Path(__file__).parent / "txt_file_for_testing2.txt"
 #     assert test_txt_file_path.exists()
-#     content = rhf.read_txt_file0(test_txt_file_path)
+#     content = rf.read_txt_file0(test_txt_file_path)
 #     assert isinstance(content, dict)
 #     assert content == txt_file_content2
 
 # def test_read_txt_file03(txt_file_content3):
 #     test_txt_file_path = Path(__file__).parent / "txt_file_for_testing3.txt"
 #     assert test_txt_file_path.exists()
-#     content = rhf.read_txt_file0(test_txt_file_path)
+#     content = rf.read_txt_file0(test_txt_file_path)
 #     assert isinstance(content, dict)
 #     assert content == txt_file_content3
 
 # def test_read_txt_file0_list0(txt_file_content_list0):
 #     test_txt_file_path = Path(__file__).parent / "txt_file_for_testing_list0.txt"
 #     assert test_txt_file_path.exists()
-#     content = rhf.read_txt_file0(test_txt_file_path)
+#     content = rf.read_txt_file0(test_txt_file_path)
 #     assert isinstance(content, list)
 #     assert content == txt_file_content_list0
 
 # def test_read_txt_file0_list1(txt_file_content_list1):
 #     test_txt_file_path = Path(__file__).parent / "txt_file_for_testing_list1.txt"
 #     assert test_txt_file_path.exists()
-#     content = rhf.read_txt_file0(test_txt_file_path)
+#     content = rf.read_txt_file0(test_txt_file_path)
 #     assert isinstance(content, list)
 #     assert content == txt_file_content_list1
 
 
-def test_read_txt_file0(txt_file_content0):
+def test_read_txt_file0(text_encoding, txt_file_content0):
     test_txt_file_path = Path(__file__).parent / "txt_file_for_testing0.txt"
     assert test_txt_file_path.exists()
-    content = rhf.read_txt_file(test_txt_file_path)
+    content = rf.read_txt_file(test_txt_file_path, text_encoding)
     assert isinstance(content, dict)
     assert content == txt_file_content0
 
 
-def test_read_txt_file1(txt_file_content1):
+def test_read_txt_file1(text_encoding, txt_file_content1):
     test_txt_file_path = Path(__file__).parent / "txt_file_for_testing1.txt"
     assert test_txt_file_path.exists()
-    content = rhf.read_txt_file(test_txt_file_path)
+    content = rf.read_txt_file(test_txt_file_path, text_encoding)
     assert isinstance(content, dict)
     assert content == txt_file_content1
 
-def test_read_txt_file2(txt_file_content2):
+def test_read_txt_file2(text_encoding, txt_file_content2):
     test_txt_file_path = Path(__file__).parent / "txt_file_for_testing2.txt"
     assert test_txt_file_path.exists()
-    content = rhf.read_txt_file(test_txt_file_path)
+    content = rf.read_txt_file(test_txt_file_path, text_encoding)
     assert isinstance(content, dict)
     assert content == txt_file_content2
 
-def test_read_txt_file3(txt_file_content3):
+def test_read_txt_file3(text_encoding, txt_file_content3):
     test_txt_file_path = Path(__file__).parent / "txt_file_for_testing3.txt"
     assert test_txt_file_path.exists()
-    content = rhf.read_txt_file(test_txt_file_path)
+    content = rf.read_txt_file(test_txt_file_path, text_encoding)
     assert isinstance(content, dict)
     assert content == txt_file_content3
 
-def test_read_txt_file_list0(txt_file_content_list0):
+def test_read_txt_file_list0(text_encoding, txt_file_content_list0):
     test_txt_file_path = Path(__file__).parent / "txt_file_for_testing_list0.txt"
     assert test_txt_file_path.exists()
-    content = rhf.read_txt_file(test_txt_file_path)
+    content = rf.read_txt_file(test_txt_file_path, text_encoding)
     assert isinstance(content, list)
     assert content == txt_file_content_list0
 
-def test_read_txt_file_list1(txt_file_content_list1):
+def test_read_txt_file_list1(text_encoding, txt_file_content_list1):
     test_txt_file_path = Path(__file__).parent / "txt_file_for_testing_list1.txt"
     assert test_txt_file_path.exists()
-    content = rhf.read_txt_file(test_txt_file_path)
+    content = rf.read_txt_file(test_txt_file_path, text_encoding)
     assert isinstance(content, list)
     assert content == txt_file_content_list1
 
-def test_read_txt_file_list2(txt_file_content_list2):
+def test_read_txt_file_list2(text_encoding, txt_file_content_list2):
     test_txt_file_path = Path(__file__).parent / "txt_file_for_testing_list2.txt"
     assert test_txt_file_path.exists()
-    content = rhf.read_txt_file(test_txt_file_path)
+    content = rf.read_txt_file(test_txt_file_path, text_encoding)
     assert isinstance(content, list)
     assert content == txt_file_content_list2
