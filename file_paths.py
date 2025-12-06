db5a65e0808644b45496efe4fda6020e6d2c13df
Heikki Jylhä <hjjylh@gmail.com>
2025-12-06 14:59:48 +0200

    made aod_path a parameter in all file_paths functions

--- file_paths.py
+++ file_paths.py
@@ -11,7 +11,6 @@ scenario_file_paths_file = "scenario_file_paths.csv"
 DEFAULT_SCENARIO = "1933.eug"
 
 
-
 def get_aod_path():
     if os.name == "nt":
         with open(this_files_directory / gamepath_in, "r") as f:
@@ -19,17 +18,20 @@ def get_aod_path():
     with open(this_files_directory / gamepath_in_linux, "r") as f:
         return Path(f.read().strip())
 
-def get_db_folder_path():
-    return get_aod_path() / "db"
+AOD_PATH = get_aod_path()
 
-def get_config_folder_path():
-    return get_aod_path() / "config"
 
-def get_scenarios_folder_path():
-    return get_aod_path() / "scenarios"
+def get_db_folder_path(aod_path=AOD_PATH):
+    return aod_path / "db"
 
-def get_tech_path():
-    return get_db_folder_path() / "tech"
+def get_config_folder_path(aod_path=AOD_PATH):
+    return aod_path / "config"
+
+def get_scenarios_folder_path(aod_path=AOD_PATH):
+    return aod_path / "scenarios"
+
+def get_tech_path(aod_path=AOD_PATH):
+    return get_db_folder_path(aod_path) / "tech"
 
 
 def get_tech_files(tech_path):
@@ -40,66 +42,66 @@ def get_tech_team_files(tech_path):
     tech_team_path = tech_path / "teams"
     return tech_team_path.glob("teams*.csv")
 
-def get_scenario_paths():
-    scenario_folder_path = get_scenarios_folder_path()
+def get_scenario_paths(aod_path=AOD_PATH):
+    scenario_folder_path = get_scenarios_folder_path(aod_path)
     scenario33_path = scenario_folder_path / "1933"
     scenario34_path = scenario_folder_path / "1934"
     return [scenario33_path, scenario34_path]
 
 # this is not always correct
-def get_scenario_path_for_country(country_code):
-    scenario_directories = get_scenario_paths()
+def get_scenario_path_for_country(country_code, aod_path=AOD_PATH):
+    scenario_directories = get_scenario_paths(aod_path)
     for sd_path in scenario_directories:
         possible_path = sd_path / f"{country_code.lower()}_{sd_path.stem[-2:]}.inc"
         if possible_path.exists():
             return possible_path
 
-def get_misc_path():
-    return get_db_folder_path() / "misc.txt"
+def get_misc_path(aod_path=AOD_PATH):
+    return get_db_folder_path(aod_path) / "misc.txt"
 
-def get_difficulty_path():
-    return get_db_folder_path() / "difficulty.csv"
+def get_difficulty_path(aod_path=AOD_PATH):
+    return get_db_folder_path(aod_path) / "difficulty.csv"
 
-def get_ministers_folder_path():
-    return get_db_folder_path() / "ministers"
+def get_ministers_folder_path(aod_path=AOD_PATH):
+    return get_db_folder_path(aod_path) / "ministers"
 
-def get_minister_modifier_path():
-    return get_ministers_folder_path() / "minister_modifiers.txt"
+def get_minister_modifier_path(aod_path=AOD_PATH):
+    return get_ministers_folder_path(aod_path) / "minister_modifiers.txt"
 
-def get_ideas_path():
-    return get_db_folder_path() / "ideas" / "ideas.txt"
+def get_ideas_path(aod_path=AOD_PATH):
+    return get_db_folder_path(aod_path) / "ideas" / "ideas.txt"
 
-def get_ministers_path(country_code):
-    return get_ministers_folder_path() / f"ministers_{country_code.lower()}.csv"
+def get_ministers_path(country_code, aod_path=AOD_PATH):
+    return get_ministers_folder_path(aod_path) / f"ministers_{country_code.lower()}.csv"
 
-def get_ministers_files():
-    return get_ministers_folder_path().glob("ministers_*.csv")
+def get_ministers_files(aod_path=AOD_PATH):
+    return get_ministers_folder_path(aod_path).glob("ministers_*.csv")
 
-def get_policies_path():
-    return get_db_folder_path() / "province_rev.inc"
+def get_policies_path(aod_path=AOD_PATH):
+    return get_db_folder_path(aod_path) / "province_rev.inc"
 
-def get_tech_names_path():
-    return get_config_folder_path() / "tech_names.csv"
+def get_tech_names_path(aod_path=AOD_PATH):
+    return get_config_folder_path(aod_path) / "tech_names.csv"
 
-def get_country_names_path():
-    return get_config_folder_path() / "world_names.csv"
+def get_country_names_path(aod_path=AOD_PATH):
+    return get_config_folder_path(aod_path) / "world_names.csv"
 
-def get_policy_names_path():
-    return get_config_folder_path() / "new_text.csv"
+def get_policy_names_path(aod_path=AOD_PATH):
+    return get_config_folder_path(aod_path) / "new_text.csv"
 
-def get_government_titles_path():
-    return get_config_folder_path() / "text.csv"
+def get_government_titles_path(aod_path=AOD_PATH):
+    return get_config_folder_path(aod_path) / "text.csv"
 
-def get_idea_titles_path():
-    return get_config_folder_path() / "boostertext.csv"
+def get_idea_titles_path(aod_path=AOD_PATH):
+    return get_config_folder_path(aod_path) / "boostertext.csv"
 
-def get_save_game_path():
-    return get_scenarios_folder_path() / "save games"
+def get_save_game_path(aod_path=AOD_PATH):
+    return get_scenarios_folder_path(aod_path) / "save games"
 
-def get_save_games():
-    save_game_folder = get_save_game_path()
+def get_save_games(aod_path=AOD_PATH):
+    save_game_folder = get_save_game_path(aod_path)
     return save_game_folder.glob("*.eug")
 
-def get_scenarios():
-    scenario_folder = get_scenarios_folder_path()
+def get_scenarios(aod_path=AOD_PATH):
+    scenario_folder = get_scenarios_folder_path(aod_path)
     return scenario_folder.glob("*.eug")
--- tests/test_file_paths.py
+++ tests/test_file_paths.py
@@ -15,27 +15,27 @@ def test_get_aod_path(aod_path):
 
 def test_get_db_folder_path(aod_path):
     if aod_path.exists():
-        assert fp.get_db_folder_path().exists()
+        assert fp.get_db_folder_path(aod_path).exists()
 
 
 def test_get_config_folder_path(aod_path):
     if aod_path.exists():
-        assert fp.get_config_folder_path().exists()
+        assert fp.get_config_folder_path(aod_path).exists()
 
 
 def test_get_scenarios_folder_path(aod_path):
     if aod_path.exists():
-        assert fp.get_scenarios_folder_path().exists()
+        assert fp.get_scenarios_folder_path(aod_path).exists()
 
 
 def test_get_tech_path(aod_path):
     if aod_path.exists():
-        assert fp.get_tech_path().exists()
+        assert fp.get_tech_path(aod_path).exists()
 
 
 def test_get_tech_files(aod_path):
     if aod_path.exists():
-        tech_path = fp.get_tech_path()
+        tech_path = fp.get_tech_path(aod_path)
         industry_tech_path = tech_path / "industry_tech.txt"
         tech_files = fp.get_tech_files(tech_path)
         assert industry_tech_path in tech_files
@@ -43,7 +43,7 @@ def test_get_tech_files(aod_path):
 
 def test_get_tech_team_files(aod_path):
     if aod_path.exists():
-        tech_path = fp.get_tech_path()
+        tech_path = fp.get_tech_path(aod_path)
         uk_tech_teams_path = tech_path / "teams" / "teams_eng.csv"
         tech_team_files = fp.get_tech_team_files(tech_path)
         assert uk_tech_teams_path in tech_team_files
@@ -51,7 +51,7 @@ def test_get_tech_team_files(aod_path):
 
 def test_get_scenario_paths(aod_path):
     if aod_path.exists():
-        path_33, path_34 = fp.get_scenario_paths()
+        path_33, path_34 = fp.get_scenario_paths(aod_path)
         assert path_33.exists()
         assert path_34.exists()
 
@@ -59,83 +59,83 @@ def test_get_scenario_paths(aod_path):
 def test_get_scenario_path_for_country(aod_path):
     if aod_path.exists():
         for code in ("AFG", "ENG", "FIN", "ITA", "SOV", "USA"):
-            assert fp.get_scenario_path_for_country(code).exists()
+            assert fp.get_scenario_path_for_country(code, aod_path).exists()
 
 
 def test_get_misc_path(aod_path):
     if aod_path.exists():
-        assert fp.get_misc_path().exists()
+        assert fp.get_misc_path(aod_path).exists()
 
 
 def test_get_difficulty_path(aod_path):
     if aod_path.exists():
-        assert fp.get_difficulty_path().exists()
+        assert fp.get_difficulty_path(aod_path).exists()
 
 
 def test_get_ministers_folder_path(aod_path):
     if aod_path.exists():
-        assert fp.get_ministers_folder_path().exists()
+        assert fp.get_ministers_folder_path(aod_path).exists()
 
 
 def test_get_minister_modifier_path(aod_path):
     if aod_path.exists():
-        assert fp.get_minister_modifier_path().exists()
+        assert fp.get_minister_modifier_path(aod_path).exists()
 
 
 def test_get_ideas_path(aod_path):
     if aod_path.exists():
-        assert fp.get_ideas_path().exists()
+        assert fp.get_ideas_path(aod_path).exists()
 
 
 def test_get_ministers_path(aod_path):
     if aod_path.exists():
         for code in ("AFG", "ENG", "FIN", "ITA", "SOV", "USA"):
-            assert fp.get_ministers_path(code).exists()
+            assert fp.get_ministers_path(code, aod_path).exists()
 
 
 def test_get_ministers_files(aod_path):
     if aod_path.exists():
-        ministers_folder_path = fp.get_ministers_folder_path()
+        ministers_folder_path = fp.get_ministers_folder_path(aod_path)
         uk_ministers = ministers_folder_path / "ministers_eng.csv"
-        assert uk_ministers in fp.get_ministers_files()
+        assert uk_ministers in fp.get_ministers_files(aod_path)
 
 
 def test_get_policies_path(aod_path):
     if aod_path.exists():
-        assert fp.get_policies_path().exists()
+        assert fp.get_policies_path(aod_path).exists()
 
 
 def test_get_tech_names_path(aod_path):
     if aod_path.exists():
-        assert fp.get_tech_names_path().exists()
+        assert fp.get_tech_names_path(aod_path).exists()
 
 
 def test_get_country_names_path(aod_path):
     if aod_path.exists():
-        assert fp.get_country_names_path().exists()
+        assert fp.get_country_names_path(aod_path).exists()
 
 
 def test_get_policy_names_path(aod_path):
     if aod_path.exists():
-        assert fp.get_policy_names_path().exists()
+        assert fp.get_policy_names_path(aod_path).exists()
 
 
 def test_get_government_titles_path(aod_path):
     if aod_path.exists():
-        assert fp.get_government_titles_path().exists()
+        assert fp.get_government_titles_path(aod_path).exists()
 
 
 def test_get_idea_titles_path(aod_path):
     if aod_path.exists():
-        assert fp.get_idea_titles_path().exists()
+        assert fp.get_idea_titles_path(aod_path).exists()
 
 
 def test_get_save_game_path(aod_path):
     if aod_path.exists():
-        assert fp.get_save_game_path().exists()
+        assert fp.get_save_game_path(aod_path).exists()
 
 
 def test_get_scenarios(aod_path):
     if aod_path.exists():
-        default_scenario_path = fp.get_scenarios_folder_path() / fp.DEFAULT_SCENARIO
-        assert default_scenario_path in fp.get_scenarios()
+        default_scenario_path = fp.get_scenarios_folder_path(aod_path) / fp.DEFAULT_SCENARIO
+        assert default_scenario_path in fp.get_scenarios(aod_path)
